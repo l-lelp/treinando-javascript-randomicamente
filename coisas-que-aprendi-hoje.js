9570ee082e87990b390685db582d955b3d467859
Joao França <francapedro10@gmail.com>
2022-10-11 17:33:16 -0300

    apenas aprendi o BÁSICO

---
+++ coisas-que-aprendi-hoje.js
@@ -0,0 +1,11 @@
+// Things that I learned today!
+// First of all, u have to open "inspection"
+
+console > alert (popup)
+
+let js = 'amazing'
+if js = 'amazing' alert("javascript is FUN!")
+
+// If I say that JS is 'boring' isnt work, cause I chose the option
+// The inspection can be used for calculate numbers
+// '+' '-''/''*''**'
