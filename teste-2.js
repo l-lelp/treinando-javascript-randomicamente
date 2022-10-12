71d452c71201ab7b0c3ce9910b530f6d15a2e05f
Joao França <francapedro10@gmail.com>
2022-10-11 21:36:25 -0300

    pelo menos eu treinei alguma coisa

---
+++ teste-2.js
@@ -0,0 +1,20 @@
+let amarelo = 15
+let vermelho = 20**2
+let azul = 30**5
+let roxo = 40**20
+let lilas = 0*0
+let laranja = 15-4
+let x = 40
+let y = 2000000**-4
+
+console.log (laranja*vermelho-roxo+lilas)/100000000000000*44440
+
+const tipoDeAmarelo = typeof(amarelo)
+const tipoDeAzul = typeof(azul)
+
+let h  = 400
+let g = 14**177
+
+console.log(amarelo**azul)
+console.log(tipoDeAmarelo)/2
+console.log(tipoDeAzul)/3
