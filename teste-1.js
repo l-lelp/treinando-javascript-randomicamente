f0863bb1d0a47f3f6f34e99b8466d41b555cf3c1
Joao França <francapedro10@gmail.com>
2022-10-11 21:18:53 -0300

    ?

---
+++ teste-1.js
@@ -0,0 +1,14 @@
+let amarelo = 15
+let vermelho = 20**2
+let azul = 30**5
+let roxo = 400000**20000030000**100
+let lilas = 0*0
+let laranja = 15-4
+
+
+const tipoDeamarelo = amarelo
+const tipoDeazul = lilas
+
+console.log(typeof(tipoDeamarelo + tipoDeazul))
+console.log(amarelo + vermelho)
+
